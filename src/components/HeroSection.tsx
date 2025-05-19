b1458e3e67e5dc59ecfa5a94a8c0e0bda3b2b3cc
Priyanka Soni <140409655+priyankasoni930@users.noreply.github.com>
2025-05-19 13:46:08 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
   const handleResumeClick = (e) => {
     e.preventDefault();
     window.open(
-      "https://drive.google.com/file/d/1taP9O2yztH5UdYtYjUt76vsP5EfEMO7p/view?usp=sharing",
+      "https://drive.google.com/file/d/1ETVU_DnsOPTY4MWKsCUXzkF7eyklkg85/view?usp=sharing",
       "_blank"
     );
   };
